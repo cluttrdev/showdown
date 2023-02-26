f207189c1c3ec08616198e4960c54aad0d5dca2a
cluttrdev <andreas@cluttr.tech>
2023-02-26 11:46:09 +0100

    Move client js to separate script

---
+++ web/static/js/client.js
@@ -0,0 +1,18 @@
+var ws = new WebSocket("ws://" + window.location.host + "/ws");
+
+ws.onmessage = (event) => {
+    var msg = JSON.parse(event.data);
+    switch (msg.type) {
+        case 'title':
+            document.querySelector('title').innerHTML = msg.data;
+            break;
+        case 'content':
+            document.querySelector('.markdown-content').innerHTML = msg.data;
+            break;
+    }
+}
+
+ws.onerror = (event) => {
+    console.debug(event)
+}
+
